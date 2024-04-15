481fc71a83c0fa127675bd103ca7d13f1b2b4ae3
Bruno Ferrari <104033731+brunoagrodev@users.noreply.github.com>
2024-04-15 20:41:32 -0300

    Update conexaoDataBaseV2.php

--- controller/conexaoDataBaseV2.php
+++ controller/conexaoDataBaseV2.php
@@ -1,10 +1,10 @@
 <?php
 //dados do banco de dados para fazer a conexão com o sistema
-$servername = "179.188.16.2";
-$username = "frutasbrasil";
-$password = "dZ8S35cnCi#5!q";
-$my_db = "frutasbrasil";
-$port = "3306";
+$servername = "";
+$username = "";
+$password = "";
+$my_db = "";
+$port = "";
 
 //Conexao com o banco de dados
 $conn = new mysqli($servername, $username, $password,$my_db, $port);
